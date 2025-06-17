bda576036bf562890d743bd26dbb0e9c2c72a594
Your Name <your-email@example.com>
2025-06-17 17:57:04 -0600

    Initial commit

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,3 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/
- */
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-// You can delete this file if you're not using it
